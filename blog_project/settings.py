4fc6ebfeed630e0cd59399805443ef144c14a7d9
Brian Getenga <briangetenga3@gmail.com>
2025-11-25 11:39:57 +0300

    Fixed ALLOWED_HOSTS for Render deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
