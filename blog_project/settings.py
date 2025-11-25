ac3e210c43800651ee48e0eac2e403bec0be9005
Brian Getenga <briangetenga3@gmail.com>
2025-11-25 12:56:42 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -33,7 +33,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.management.utils import get_random_secret_key
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ['SECRET_KEY']  # no fallback!
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
